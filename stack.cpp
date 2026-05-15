c654a2dc3e157fc621021a7a68f1119c4fff6892
raffibromabar-web <raffibromabar@gmail.com>
2026-05-15 14:48:21 +0700

    Menyambungkan data baru ke tumpukan dan memperbarui posisi top

--- stack.cpp
+++ stack.cpp
@@ -32,4 +32,9 @@ public:
     {
         Node *newNode = new Node();
         newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
 };
